e55bfc1a9f4f2586dea83cc278f80ee6e9f7c382
cs24s013-beep <cs24s013@smail.iitm.ac.in>
2026-09-17 20:43:03 +0530

    Fix SportsCar constructor arguments and main syntax in abstraction.cpp

---
+++ OOps/abstraction.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+// now we are going to create a abstract class which will be inherited by child class and everything will be defined there
+class Car {
+public:
+    virtual void startEngine() = 0;  // Pure virtual function
+    virtual void shiftGear(int gear) = 0;
+    virtual void accelerate() = 0;
+    virtual void brake() = 0;
+    virtual void stopEngine() = 0;
+    virtual ~Car() {}                // Virtual destructor
+};
+//the =0 tells that this is pure abstract class, any derived class need to explain every function oterwise that also remains abstract.
+//virtual ~Car() {}   is vitual destructor to prevent memory leak, Now we will create some subclass and inherit  its property.
+class SportsCar : public Car {
+public:
+    string brand;
+    string model;
+    bool isEngineOn;
+    int currentSpeed;
+    int currentGear;
+
+    SportsCar(string b, string m) {
+        this->brand = b;
+        this->model = m;
+        isEngineOn = false;
+        currentSpeed = 0;
+        currentGear = 0;
+    }
+
+    void startEngine() {
+        isEngineOn = true;
+        cout << brand << " " << model << " : Engine starts with a roar!" << endl;
+    }
+
+    void shiftGear(int gear) {
+        if (!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot Shift Gear." << endl;
+            return;
+        }
+        currentGear = gear;
+        cout << brand << " " << model << " : Shifted to gear " << currentGear << endl;
+    }
+
+    void accelerate() {
+        if (!isEngineOn) {
+            cout << brand << " " << model << " : Engine is off! Cannot accelerate." << endl;
+            return;
+        }
+        currentSpeed += 20;
+        cout << brand << " " << model << " : Accelerating to " << currentSpeed << " km/h" << endl;
+    }
+
+    void brake() {
+        currentSpeed -= 20;
+        if (currentSpeed < 0) currentSpeed = 0;
+        cout << brand << " " << model << " : Braking! Speed is now " << currentSpeed << " km/h" << endl;
+    }
+
+    void stopEngine() {
+        isEngineOn = false;
+        currentGear = 0;
+        currentSpeed = 0;
+        cout << brand << " " << model << " : Engine turned off." << endl;
+    }
+};
+//Main
+   int main() {
+
+    Car* myCar = new SportsCar("Ford", "Mustang");
+
+    myCar->startEngine();
+    myCar->shiftGear(1);
+    myCar->accelerate();
+    myCar->shiftGear(2);
+    myCar->accelerate();
+    myCar->brake();
+    myCar->stopEngine();
+
+    delete myCar;    
+
+    return 0;
+}
